69c7e38ac94e591fdd4aeca61ac3a62c04ce21ba
Michael Silva <michael.steam@outlook.com>
2022-03-08 09:02:19 -0300

    Adicionar arquivos de projeto.

---
+++ Ex3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0;
+            int somaPar = 0;
+            int somaImpar = 0;
+            while(n <= 1000)
+            {
+                Console.WriteLine("Digite um número: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                if(n <= 1000)
+                {
+                    if (n % 2 == 0)
+                    {
+                        somaPar += n;
+                    }
+                    else
+                    {
+                        somaImpar += n;
+                    }
+                }
+                
+                
+            }
+            Console.WriteLine("Soma dos pares: {0}", somaPar);
+            Console.WriteLine("Soma dos ímpares: {0}", somaImpar);
+
+
+
+
+
+        }
+    }
+}
